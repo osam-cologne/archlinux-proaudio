879ccbc1ec16cc5296c7355b67edd203819b4657
Florian Hülsmann <fh@cbix.de>
2022-02-17 18:58:05 +0100

    mamba package from AUR; basic build script

---
+++ tools/clean-build-all.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# makepkg -src installs and removes required dependencies so this can be run on CI,
+# for example the archlinux/archlinux:base-devel image, to build from a clean root.
+
+cd "${0%/*}/.."
+ROOT="$(pwd)"
+
+export PKGDEST="$ROOT/out"
+export LOGDEST="$ROOT/out"
+
+SUCC=0
+FAIL=0
+SKIP=0
+
+cd "$ROOT/packages"
+for PKG in *; do
+	cd $PKG
+	PKGLIST="$(makepkg --packagelist)"
+	if ls $PKGLIST 2>/dev/null 1>&2; then
+		SKIP=$((SKIP+1))
+		echo "$PKG fully built, skipping ..."
+		cd ..
+		continue
+	fi
+
+	if makepkg -srcf --noconfirm --noprogressbar; then
+		SUCC=$((SUCC+1))
+	else
+		FAIL=$((FAIL+1))
+		rm -f $PKGLIST
+	fi
+	cd ..
+done
+echo "$SUCC built, $FAIL failed, $SKIP skipped"
+[ $FAIL -gt 0 ] && exit 1
