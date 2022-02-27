4fb45986af7e56e86c8ad262f33eb0e5be2a460e
Florian Hülsmann <fh@cbix.de>
2022-02-27 14:11:26 +0100

    proper package dirs; remove gtk2 split package

--- packages/tuxguitar/tuxguitar.sh
+++ packages/tuxguitar/tuxguitar.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -e
 export PATH="/usr/lib/jvm/java-11-openjdk/bin:$PATH"
-for jar in /opt/tuxguitar/lib/*.jar; do
+for jar in /usr/share/java/tuxguitar/*.jar; do
   CP="$CP:$jar"
 done
 export SWT_GTK3=1
 exec java -cp "$CP" \
-  -Dtuxguitar.home.path=/opt/tuxguitar \
-  -Dtuxguitar.share.path=/opt/tuxguitar/share/ \
-  -Djava.library.path="/opt/tuxguitar/lib:$LD_LIBRARY_PATH" \
+  -Dtuxguitar.home.path=/usr/share/tuxguitar \
+  -Dtuxguitar.share.path=/usr/share/tuxguitar/share \
+  -Djava.library.path=/usr/lib/tuxguitar \
   org.herac.tuxguitar.app.TGMainSingleton $*
