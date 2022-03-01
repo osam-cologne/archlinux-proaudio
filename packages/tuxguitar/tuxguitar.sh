5653d634d4a8dcb8901d5c8b6497574fc05ddf96
Florian Hülsmann <fh@cbix.de>
2022-03-01 23:02:04 +0100

    Package TuxGuitar (#4)
    * tuxguitar: PKGBUILD from AUR, reproducible build patches
    
    * pin openjdk 11 to fix playback
    
    * maven config, install dir, custom launch script
    
    * create patch, SWT 4.22
    
    * proper package dirs; remove gtk2 split package
    
    * fix docs and conflicts
    
    * get docs from classpath
    
    * remove included sf2 file due to missing license
    
    * refactor dependencies
    
    * cleanup; fix architectures
    
    * use arch-specific sources for swt
    
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>
    
    * formatting
    
    Co-authored-by: Christopher Arndt <chris@chrisarndt.de>

---
+++ packages/tuxguitar/tuxguitar.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+export PATH="/usr/lib/jvm/java-11-openjdk/bin:$PATH"
+for jar in /usr/share/java/tuxguitar/*.jar; do
+  CP="${CP}:$jar"
+done
+CP="${CP}:/usr/share/tuxguitar/dist:/usr/share/doc/tuxguitar"
+export SWT_GTK3=1
+exec java -cp "$CP" \
+  -Dtuxguitar.home.path=/usr/share/tuxguitar \
+  -Dtuxguitar.share.path=/usr/share/tuxguitar/share \
+  -Djava.library.path=/usr/lib/tuxguitar \
+  org.herac.tuxguitar.app.TGMainSingleton $*
