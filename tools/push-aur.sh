4a41ac45d1e5c448270cba8918572950a2e9f9ed
Christopher Arndt <SpotlightKid@users.noreply.github.com>
2022-11-01 15:45:02 +0100

    fix: copy .ssh to correct dest dir for push-aur script (#145)

--- tools/push-aur.sh
+++ tools/push-aur.sh
@@ -4,30 +4,35 @@ if [ -z $CI ]; then
     exit 1
 fi
 
+set -e
+
 cd "${0%/*}/.."
 ROOT="$(pwd)"
-TMP="${ROOT}/.tmp"
+TMP="$ROOT/.tmp"
 
 sudo pacman -S --noconfirm git openssh
 
-PACKAGES="$(cd .tmp/pkgs; ls -1)"
+PACKAGES="$(cd "$TMP"/pkgs; ls -1)"
 for PKG in $PACKAGES; do
     if ! grep -Fxq $PKG "$ROOT"/aur/packages; then
         continue
     fi
     echo "Updating AUR package $PKG ..."
     cd "$TMP"/aur
+    echo "Cloning AUR repository for $PKG ..."
     git clone -n ssh://aur.archlinux.org/$PKG.git
     cp -a "$ROOT"/packages/$PKG .
     cd $PKG
 
     # generate .SRCINFO
+    echo "Generating .SRCINFO for $PKG ..."
     sudo -Eu nobody makepkg --printsrcinfo > .SRCINFO
 
     # update .gitignore
     cat "$ROOT"/aur/gitignore-base >> .gitignore
 
     # commit and push
+    echo "Committing and pushing new version of $PKG to AUR ..."
     git add .
     git commit --author="$PACKAGER" -m "$DRONE_COMMIT_MESSAGE"
     git push
