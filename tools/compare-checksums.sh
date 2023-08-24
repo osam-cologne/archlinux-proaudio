0c4ee4a4fe46ebe704ce5babf9de7774b8b71582
Christopher Arndt <chris@chrisarndt.de>
2023-08-24 15:33:24 +0200

    [ci] feat: allow ignoring reproducible build failures
    * Fix package updating in prepare build step
    
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

---
+++ tools/compare-checksums.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Compare SHA checksum of built packages
+
+res=0
+
+PACKAGES="$(cd .tmp/pkgs; ls -1)"
+
+for PACKAGE in $PACKAGES; do
+    if [[ ! -f "packages/$PACKAGE/PKGBUILD" ]]; then
+        continue
+    fi
+
+    if [[ ! -f "packages/$PACKAGE/.no-checksum" ]]; then
+        PKGFILES=($(cd packages/$PACKAGE; makepkg --packagelist))
+
+        for PKG in "${PKGFILES[@]}"; do
+            echo "Comparing checksum of out/${PKG##*/}..."
+            cd out
+            sha512sum "${PKG##*/}" > "${TMP:-/tmp}/${PKG##*/}".sha512
+            cd ../out2
+            sha512sum -c "${TMP:-/tmp}/${PKG##*/}".sha512
+
+            if [[ $? -ne 0 ]]; then
+               res=1
+            fi
+        done
+    else
+        echo "Skipping checksum check of package $PACKAGE."
+    fi
+done
+
+exit $res
---
+++ tools/diff-packages.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Compare built packages to check for reproducible build
+
+res=0
+
+test -n "$PACKAGES" || exit 0
+
+for PACKAGE in $PACKAGES; do
+    if [[ ! -f "packages/$PACKAGE/PKGBUILD" ]]; then
+        continue
+    fi
+
+    if [[ ! -f "packages/$PACKAGE/.no-diffoscope" ]]; then
+        PKGFILES=($(cd packages/$PACKAGE; makepkg --packagelist))
+
+        for PKG in "${PKGFILES[@]}"; do
+            if [[ "$PKG" =~ '-debug-' ]]; then
+                continue
+            fi
+
+            echo "Diffing out/${PKG##*/} and out2/${PKG##*/}..."
+            diffoscope \
+                --text - \
+                --max-text-report-size 51200 \
+                out/"${PKG##*/}" \
+                out2/"${PKG##*/}"
+
+            if [[ $? -ne 0 ]]; then
+               res=1
+            fi
+        done
+    else
+        echo "Skipping diffoscope check of package $PACKAGE."
+    fi
+done
+
+exit $res
