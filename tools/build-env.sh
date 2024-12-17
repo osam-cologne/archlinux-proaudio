069328113ba8cee3eefa3e87a9eb3e83d321bff3
Christopher Arndt <chris@chrisarndt.de>
2024-12-17 21:16:20 +0100

    [ci] fix: disable 'nobody' account expiration
    sudo/pam refuses to work for expired accounts
    
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- tools/build-env.sh
+++ tools/build-env.sh
@@ -3,10 +3,12 @@ if [ -z $CI ]; then
     echo "Only run in CI"
     exit 1
 fi
-echo '%nobody ALL=(ALL:ALL) NOPASSWD: ALL' > /etc/sudoers.d/nobody-sudo
+
 cat tools/pacman.conf >> /etc/pacman.conf
 cat tools/makepkg.conf >> /etc/makepkg.conf
+
 source /etc/makepkg.conf
+
 export PACKAGER
 export SRCDEST=${SRCDEST:-/tmp/build}
 export SRCPKGDEST=/tmp/build
@@ -15,7 +17,14 @@ export PKGDEST=${PKGDEST:-/tmp/build}
 export LOGDEST=${LOGDEST:-$PKGDEST}
 export SOURCE_DATE_EPOCH=${DRONE_BUILD_CREATED:-${DRONE_BUILD_STARTED:-$(date +%s)}}
 export HOME=/tmp
+
 mkdir -p .tmp/pkgs
+
+echo "Enabling sudo for user nobody"
+# disable account expiration
+usermod -e "" nobody
+echo '%nobody ALL=(ALL:ALL) NOPASSWD: ALL' > /etc/sudoers.d/nobody-sudo
 echo "Fixing sudo permissions"
 chmod u+s /usr/sbin/sudo
+
 alias run_nobody='PACKAGES="$(cd .tmp/pkgs; ls -1)" sudo -u nobody --preserve-env=HOME,CI,PACKAGER,PKGDEST,LOGDEST,SRCDEST,SRCPKGDEST,BUILDDIR,PACKAGE,PACKAGES,SOURCE_DATE_EPOCH,IGNOREDB,DRONE_TARGET_BRANCH,MAKEPKG_ARGS,MAKEFLAGS,DRONE_COMMIT_MESSAGE,PKGEXT,DRONE_COMMIT_BEFORE,DRONE_COMMIT_AFTER'
