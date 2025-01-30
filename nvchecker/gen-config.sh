6ec380695344d77effb8b0b347ddee8b0321c54f
Florian Hülsmann <fh@cbix.de>
2025-01-30 22:30:49 +0100

    Split up nvchecker config

---
+++ nvchecker/gen-config.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd "${0%/*}/.."
+ROOT="$(pwd)"
+
+CFG=nvchecker/archlinux-proaudio.toml
+VER=nvchecker/old_ver.json
+cat > $CFG << __EOF__
+[__config__]
+oldver = "old_ver.json"
+newver = "new_ver.json"
+keyfile = "keyfile.toml"
+
+__EOF__
+
+VER_TEMP="$(mktemp)"
+for f in packages/*/.nvchecker.toml; do
+  cat $f >> $CFG
+  echo "" >> $CFG
+  pkgdir=$(dirname $f)
+  pkgbase=$(basename $pkgdir)
+  pkgver=$(. $pkgdir/PKGBUILD; echo $pkgver)
+  printf '{"%s": "%s"}\n' $pkgbase $pkgver >> "$VER_TEMP"
+done
+jq -s add "$VER_TEMP" > $VER
